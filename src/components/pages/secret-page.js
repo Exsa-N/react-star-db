da833a8a8daec4ce7888cca0b2f692dc3c59d2b9
Nikita Turkin <nexsanik@gmail.com>
2019-07-27 03:01:05 +0300

    Update secret-page.js

--- src/components/pages/secret-page.js
+++ src/components/pages/secret-page.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Redirect} from 'react-router-dom';
 
+import mp3_file from '../../star_wars.mp3';
+
 const SecretPage = ({isLoggedIn}) => {
     if(isLoggedIn)
         return (
             <div className="jumbotron text-center">
+                <audio autoPlay><source src = {mp3_file} type="audio/mpeg"/></audio>
                 <h3>This page full of secrets!!!</h3>
             </div>
         );
